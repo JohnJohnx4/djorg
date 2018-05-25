daf27689a928fe2f39fb6b2dc7821cdcfd016037
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-05-25 11:49:42 -0400

    got caught up

--- djorg-app/urls.py
+++ djorg-app/urls.py
@@ -1,5 +1,4 @@
 """djorg URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -14,8 +13,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='djorg_base.html')),
+    path('bookmarks/', include('bookmarks.urls')),
     path('admin/', admin.site.urls),
 ]
